6c08f197a375e0ca279e1473b152a0a5e06e8c49
Conner Groth <connergroth@gmail.com>
2025-05-13 12:52:57 -0600

    fixed cursor inconsistencies

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -79,9 +79,13 @@ const ProjectCategory: React.FC<{ title: string, projects: Project[] }> = ({ tit
     </h2>
     
     <div className="projects-container">
-      <div className="project-list flex flex-col md:flex-row overflow-x-auto no-scrollbar md:gap-8 cursor-grab">
+      <div className="project-list flex flex-col md:flex-row overflow-x-auto no-scrollbar md:gap-8">
         {projects.map((project, index) => (
-          <article key={index} className="flex mx-2 md:mx-0 my-10 rounded-lg bg-[#070a0d] bg-opacity-60 shadow">
+          <article 
+            key={index} 
+            className="flex mx-2 md:mx-0 my-10 rounded-lg bg-[#070a0d] bg-opacity-60 shadow cursor-pointer hover:bg-opacity-70 transition-all duration-300 select-none"
+            draggable="false"
+          >
             <div className="flex flex-col md:w-[22rem]">
               <img 
                 src={project.image} 
@@ -105,7 +109,7 @@ const ProjectCategory: React.FC<{ title: string, projects: Project[] }> = ({ tit
                   
                   <div className="flex flex-row justify-between">
                     {project.liveUrl && (
-                      <a className="learn-more-btn text-xs py-1.5" href={project.liveUrl} target="_blank" rel="noopener noreferer">
+                      <a className="learn-more-btn text-xs py-1.5 cursor-pointer" href={project.liveUrl} target="_blank" rel="noopener noreferer">
                         <svg className="h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                           <path fill="currentColor" d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
                         </svg>
@@ -114,7 +118,7 @@ const ProjectCategory: React.FC<{ title: string, projects: Project[] }> = ({ tit
                     )}
                     
                     {project.githubUrl && (
-                      <a className="learn-more-btn text-xs py-1.5" href={project.githubUrl} target="_blank" rel="noopener noreferer">
+                      <a className="learn-more-btn text-xs py-1.5 cursor-pointer" href={project.githubUrl} target="_blank" rel="noopener noreferer">
                         <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="h-3.5 w-3.5">
                           <title>GitHub</title>
                           <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
